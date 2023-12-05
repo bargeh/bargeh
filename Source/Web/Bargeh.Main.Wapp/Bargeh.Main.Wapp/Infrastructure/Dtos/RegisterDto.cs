45f620ef114529ad8580a576a3dbc0b051f41b6d
Matin mohammadi <matinmn87@gmail.com>
2023-12-05 19:20:10 +0330

    Fix issues with login form validation

--- Source/Web/Bargeh.Main.Wapp/Bargeh.Main.Wapp/Infrastructure/Dtos/RegisterDto.cs
+++ Source/Web/Bargeh.Main.Wapp/Bargeh.Main.Wapp/Infrastructure/Dtos/RegisterDto.cs
@@ -8,8 +8,10 @@ public class RegisterDto
     public string? Phone { get; set; }
 
     [Required (ErrorMessage = "لطفا یه نام کاربری برای خودت انتخاب کن")]
+    [Length (6, 20, ErrorMessage = "نام کاربری باید بین ۶ تا ۲۰ کارکتر باشه")]
     public string? Username { get; set; }
 
     [Required (ErrorMessage = "لطفا یه نام نمایشی هم برای خودت انتخاب کن")]
+    [Length (1, 30, ErrorMessage = "نام نمایشی باید بین ۱ تا ۳۰ کارکتر باشه")]
     public string? DisplayName { get; set; }
 }
