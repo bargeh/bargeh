d2bdd20bd8305d5bc2ad3796252b96ab9bf56e5a
Matin mohammadi <matinmn87@gmail.com>
2023-12-12 18:27:51 +0330

    Use Compiled Queries

--- Source/Services/Bargeh.Users.API/Infrastructure/UsersContext.cs
+++ Source/Services/Bargeh.Users.API/Infrastructure/UsersContext.cs
@@ -18,6 +18,26 @@ public class UsersContext (DbContextOptions<UsersContext> options) : DbContext (
 			(UsersContext context, string username) =>
 				context.Users.FirstOrDefault (u => u.Username == username));
 
+	private static Func<UsersContext, string, Task<User?>> _getUserByPhone =
+		EF.CompileAsyncQuery (
+			(UsersContext context, string phone) =>
+				context.Users.FirstOrDefault (u => u.PhoneNumber == phone));
+
+	private static Func<UsersContext, Guid, Task<User?>> _getUserById =
+		EF.CompileAsyncQuery (
+			(UsersContext context, Guid id) =>
+				context.Users.FirstOrDefault (u => u.Id == id));
+
+	private static Func<UsersContext, string, string, Task<User?>> _getUserByPhoneAndPassword =
+		EF.CompileAsyncQuery (
+			(UsersContext context, string phone, string password) =>
+				context.Users.FirstOrDefault (u => u.PhoneNumber == phone && u.Password == password.Hash (HashType.SHA256)));
+
+	private static Func<UsersContext, string, Task<bool>> _userExistsByPhone =
+		EF.CompileAsyncQuery (
+			(UsersContext context, string phone) =>
+				context.Users.Any (u => u.PhoneNumber == phone));
+
 	#endregion
 
 	#region Compiled Queries' Methods
@@ -25,6 +45,31 @@ public class UsersContext (DbContextOptions<UsersContext> options) : DbContext (
 	public async Task<User?> GetUserByUsernameAsync (string username)
 	{
 		return await _getUserByUsername (this, username);
+	}
+
+	public async Task<User?> GetUserByPhoneNumberAsync (string phone)
+	{
+		return await _getUserByPhone (this, phone);
+	}
+
+	public async Task<User?> GetUserByIdAsync (string? id)
+	{
+		if (string.IsNullOrWhiteSpace (id) || !Guid.TryParse (id, out Guid guid))
+		{
+			return null;
+		}
+
+		return await _getUserById (this, guid);
+	}
+
+	public async Task<User?> GetUserByPhoneAndPasswordAsync (string phone, string password)
+	{
+		return await _getUserByPhoneAndPassword (this, phone, password);
+	}
+
+	public async Task<bool> UserExistsByPhoneAsync (string phone)
+	{
+		return await _userExistsByPhone (this, phone);
 	}
 
 	#endregion
--- Source/Services/Bargeh.Users.API/Services/UserService.cs
+++ Source/Services/Bargeh.Users.API/Services/UserService.cs
@@ -36,7 +36,7 @@ public class UserService (UsersContext context) : UsersProto.UsersProtoBase
 	public override async Task<GetUserReply> GetUserByPhone (GetUserByPhoneRequest request,
 															 ServerCallContext callContext)
 	{
-		var user = await context.Users.FirstOrDefaultAsync (u => u.PhoneNumber == request.Phone);
+		var user = await context.GetUserByPhoneNumberAsync (request.Phone);
 
 		if (user == null)
 		{
@@ -58,20 +58,10 @@ public class UserService (UsersContext context) : UsersProto.UsersProtoBase
 		};
 	}
 
-	public override async Task<GetUserReply> GetUserById (GetUserByIdRequest request, ServerCallContext callContext)
+	public override async Task<GetUserReply> GetUserById (GetUserByIdRequest request,
+														  ServerCallContext callContext)
 	{
-		Guid? guid;
-
-		try
-		{
-			guid = Guid.Parse (request.Id);
-		}
-		catch (Exception)
-		{
-			throw new RpcException (new (StatusCode.InvalidArgument, "Invalid Argument"));
-		}
-
-		var user = await context.Users.FirstOrDefaultAsync (u => u.Id == guid);
+		User? user = await context.GetUserByIdAsync (request.Id);
 
 		if (user == null)
 		{
@@ -96,8 +86,7 @@ public class UserService (UsersContext context) : UsersProto.UsersProtoBase
 	public override async Task<GetUserReply> GetUserByPhoneAndPassword (GetUserByPhoneAndPasswordRequest request,
 																		ServerCallContext callContext)
 	{
-		var user = await context.Users.FirstOrDefaultAsync (u =>
-			u.PhoneNumber == request.Phone && u.Password == request.Password.Hash (HashType.SHA256));
+		var user = await context.GetUserByPhoneAndPasswordAsync (request.Phone, request.Password);
 
 		// TODO: Verify captcha
 
@@ -129,7 +118,7 @@ public class UserService (UsersContext context) : UsersProto.UsersProtoBase
 	public override async Task<VoidOperationReply> SetUserPassword (SetUserPasswordRequest request,
 																	ServerCallContext callContext)
 	{
-		var user = await context.Users.FirstOrDefaultAsync (u => u.Id == Guid.Parse (request.Id)) ?? throw new RpcException (new (StatusCode.NotFound, "Not Found"));
+		User user = await context.GetUserByIdAsync (request.Id) ?? throw new RpcException (new (StatusCode.NotFound, "Not Found"));
 
 		user.Password = request.Password;
 
@@ -143,15 +132,15 @@ public class UserService (UsersContext context) : UsersProto.UsersProtoBase
 
 	public override async Task<VoidOperationReply> AddUser (AddUserRequest request, ServerCallContext callContext)
 	{
-		var userExists =
-			await context.Users.AnyAsync (u => u.PhoneNumber == request.Phone);
+		bool userExists =
+			await context.UserExistsByPhoneAsync (request.Phone);
 
 		if (userExists)
 		{
 			throw new RpcException (new (StatusCode.AlreadyExists, "Already Exists"));
 		}
 
-		int discriminator = Random.Shared.Next (10000, 99999);
+		int discriminator = Random.Shared.Next (1000, 9999);
 
 		string username = $"user{discriminator}";
 		string displayName = "کاربر " + discriminator;
