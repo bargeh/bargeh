185781d8cccd16d9f50f5352dd95dd6220533c13
Matin mohammadi <matinmn87@gmail.com>
2023-12-31 10:51:58 +0330

    Code postgres preparer

--- Source/Services/Bargeh.Users.API.UnitTests/UnitTest1.cs
+++ Source/Services/Bargeh.Users.API.UnitTests/UnitTest1.cs
@@ -1,10 +1,31 @@
+using Bargeh.Users.API.Infrastructure;
+using Bargeh.Users.API.Models;
+using Bargeh.Users.API.Services;
+using Microsoft.EntityFrameworkCore;
+using Users.API;
+
 namespace Bargeh.Users.API.UnitTests;
 
 public class UnitTest1
 {
 	[Fact]
-	public void Test1 ()
+	public void GetUserByUsername_ReturnsCorrectUser ()
 	{
+		// Arrange
+		string connectionString = "Connection string";
+
+		DbContextOptionsBuilder<UsersContext> optionsBuilder = new();
+		optionsBuilder.UseNpgsql (connectionString);
+
+
+		UsersContext context = new(optionsBuilder.Options);
+
+
+		UserService userService = new(context);
+
+		// Act
+
+		// Assert
 
 	}
 }
---
+++ Source/Services/Bargeh.Users.API.UnitTests/UnitTestsDbProvider.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using Xunit.Abstractions;
+
+namespace Bargeh.Users.API.UnitTests;
+
+public static class UnitTestsDbProvider
+{
+	public static async Task PreparePostgresDb ()
+	{
+		try
+		{
+			await new HttpClient ().GetAsync ("http://localhost:5432");
+		}
+		catch (HttpRequestException exception)
+		{
+			if (exception.InnerException!.GetType () != typeof (HttpIOException))
+			{
+				ProcessStartInfo startInfo = new ()
+				{
+					FileName = "docker",
+					Arguments = "run --name test-postgres -e POSTGRES_PASSWORD=5 -e POSTGRES_USER=postgres -e POSTGRES_DB=postgres -p 5432:5432 -d postgres",
+					RedirectStandardOutput = true,
+					UseShellExecute = false,
+					CreateNoWindow = true,
+				};
+
+				Process process = new() { StartInfo = startInfo };
+				process.Start ();
+
+				await process.WaitForExitAsync ();
+			}
+		}
+	}
+}
