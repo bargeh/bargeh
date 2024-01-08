95c36fd9e562fe9247fef4ce8575a035c9456e8c
Matin <matinmn87@gmail.com>
2024-01-08 21:12:58 +0330

    Make names even more fluent

--- Source/Build/Bargeh.Aspire.AppHost/Program.cs
+++ Source/Build/Bargeh.Aspire.AppHost/Program.cs
@@ -4,18 +4,18 @@ IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder (a
 IResourceBuilder<PostgresContainerResource> postgres = builder.AddPostgresContainer ("postgres", 5432, "5");
 
 IResourceBuilder<ProjectResource> usersApi = builder
-	.AddProject<Projects.Bargeh_Users_Api> ("users.api")
+	.AddProject<Projects.Bargeh_Users_Api> ("users")
 	.WithReference (postgres);
 
-IResourceBuilder<ProjectResource> smsApi = builder.AddProject<Projects.Bargeh_Sms_Api> ("sms.api");
+IResourceBuilder<ProjectResource> smsApi = builder.AddProject<Projects.Bargeh_Sms_Api> ("sms");
 
-builder.AddProject<Projects.Bargeh_Main_Wapp> ("bargeh.main.wapp")
+builder.AddProject<Projects.Bargeh_Main_Wapp> ("wapp")
 	//.WithReference (sqlServer)
 	.WithReference (usersApi)
 	.WithReference (smsApi)
 	.WithLaunchProfile ("https");
 
-builder.AddProject<Projects.Bargeh_Identity_Api> ("identity.api")
+builder.AddProject<Projects.Bargeh_Identity_Api> ("identity")
 	.WithReference (usersApi)
 	.WithReference (postgres);
 
