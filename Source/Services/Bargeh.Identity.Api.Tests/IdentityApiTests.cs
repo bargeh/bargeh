046d98947ee708da22a132daf063db947e0ce6a1
Matin <matinmn87@gmail.com>
2024-01-21 13:52:00 +0330

    Update packages

--- Source/Services/Bargeh.Identity.Api.Tests/IdentityApiTests.cs
+++ Source/Services/Bargeh.Identity.Api.Tests/IdentityApiTests.cs
@@ -13,31 +13,31 @@ public class IdentityApiTests : UsersTestsBase
     private readonly TimeProvider _timeProvider = null!;
     private IdentityService _identityService = null!;
 
-    public override async Task InitializeAsync ()
-    {
-        await base.InitializeAsync ();
+    //public override async Task InitializeAsync ()
+    //{
+    //    await base.InitializeAsync ();
         
-        UsersProtoClient usersClient = Mock.Of<UsersProtoClient> ();
+    //    UsersProtoClient usersClient = Mock.Of<UsersProtoClient> ();
 
-        DbContextOptionsBuilder<IdentityDbContext> optionsBuilder = new ();
-        optionsBuilder.UseNpgsql (ConnectionString);
+    //    DbContextOptionsBuilder<IdentityDbContext> optionsBuilder = new ();
+    //    optionsBuilder.UseNpgsql (ConnectionString);
 
-        IdentityDbContext identityDbContext = new (optionsBuilder.Options);
-        await IdentityDbInitializer.InitializeDbAsync (identityDbContext, new NullLogger<IdentityDbContext> ());
+    //    IdentityDbContext identityDbContext = new (optionsBuilder.Options);
+    //    await IdentityDbInitializer.InitializeDbAsync (identityDbContext, new NullLogger<IdentityDbContext> ());
 
-        _identityService = new (usersClient, identityDbContext, _timeProvider);
-    }
+    //    _identityService = new (usersClient, identityDbContext, _timeProvider);
+    //}
 
-    [Fact]
-    public async Task Login_ThrowsIfUserIsInvalid ()
-    {
-        // Arrange
-
-
-        // Act
+    //[Fact]
+    //public async Task Login_ThrowsIfUserIsInvalid ()
+    //{
+    //    // Arrange
 
 
-        // Assert
+    //    // Act
 
-    }
+
+    //    // Assert
+
+    //}
 }
