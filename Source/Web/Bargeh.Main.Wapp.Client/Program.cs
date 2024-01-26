697f9966b688815c902bd21534749fd9e376e825
Matin <matinmn87@gmail.com>
2024-01-26 13:13:35 +0330

    Fix errors

--- Source/Web/Bargeh.Main.Wapp.Client/Program.cs
+++ Source/Web/Bargeh.Main.Wapp.Client/Program.cs
@@ -1,9 +1,13 @@
 using Bargeh.Main.Wapp.Client.Services;
+using Identity.Api;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault (args);
 
 builder.Services.AddScoped<LocalStorageService> ();
 builder.Services.AddScoped<IdentityProviderService> ();
+
+builder.Services.AddGrpcClient<IdentityProto.IdentityProtoClient> (o =>
+	o.Address = new("http://localhost:5201"));
 
 await builder.Build ().RunAsync ();
