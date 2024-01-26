346ccbeefa753f31642f7016e0236370045992bd
Matin <matinmn87@gmail.com>
2024-01-26 12:37:56 +0330

    Revert "Revert "Init IdentityProviderService""
    This reverts commit bb9e8a1a174ae2557fdffa8e556708ecd7280f4b.

---
+++ Source/Web/Bargeh.Main.Wapp/Services/IdentityProviderService.cs
@@ -0,0 +1,9 @@
+﻿namespace Bargeh.Main.Wapp.Services;
+
+public class IdentityProviderService (LocalStorageService localStorageService)
+{
+	public async Task GetUserIdentity ()
+	{
+		await localStorageService.SetItemAsync("what a?", "huh?");
+	}
+}
