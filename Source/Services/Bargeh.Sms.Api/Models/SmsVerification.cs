49d59f9eefcac649990e81353786e7a3857d14c8
Matin <matinmn87@gmail.com>
2024-02-16 10:24:26 +0330

    Fix migration

--- Source/Services/Bargeh.Sms.Api/Models/SmsVerification.cs
+++ Source/Services/Bargeh.Sms.Api/Models/SmsVerification.cs
@@ -4,5 +4,5 @@ public class SmsVerification
 {
 	public Guid Id { get; set; }
 	public required ushort Code { get; set; }
-	public required Guid UserId { get; set; }
+	public required string Phone { get; set; }
 }
