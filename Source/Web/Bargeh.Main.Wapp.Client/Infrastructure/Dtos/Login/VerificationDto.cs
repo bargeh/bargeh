3b45ab41b23df51317a1b8510fff3b5460013dfb
Matin <matinmn87@gmail.com>
2024-02-23 13:32:15 +0330

    Remove all files

--- Source/Web/Bargeh.Main.Wapp.Client/Infrastructure/Dtos/Login/VerificationDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Bargeh.Main.Wapp.Client.Infrastructure.Dtos.Login;
-
-public class VerificationDto : AuthenticationDtoBase
-{
-	[Length(4, 4, ErrorMessage = "کد تایید باید ۴ رقمی باشه")]
-	public string? VerificationCode { get; set; }
-}
