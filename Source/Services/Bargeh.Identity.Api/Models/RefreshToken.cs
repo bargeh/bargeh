3c3dce4a4df0e0c9df2d9172204deb0569a05d91
Matin <matinmn87@gmail.com>
2024-02-26 11:00:25 +0330

    Cleanup

--- Source/Services/Bargeh.Identity.Api/Models/RefreshToken.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Bargeh.Identity.Api.Models;
-
-public sealed class RefreshToken
-{
-	public Guid Id { get; init; } = new ();
-	public required string Token { get; init; }
-	public required Guid UserId { get; init; }
-	public DateTime ExpireDate { get; init; } = DateTime.UtcNow.AddDays (30);
-}
--- Source/Web/Bargeh.Main.Wapp/wwwroot/js/recaptcha.js
+++
@@ -1,9 +0,0 @@
-window.setDotNetReference = function (dotNetReference) {
-    window.dotNetReference = dotNetReference;
-}
-
-window.captchaSuccess = function (token) {
-    setTimeout(() => {
-        window.dotNetReference.invokeMethodAsync('OnCaptchaSubmit', token);
-    }, 1000)
-}
