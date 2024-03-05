efd24ffc0eb3d0b3eb6eac21fae85b4161f3fe66
Matin <matinmn87@gmail.com>
2024-03-05 15:11:43 +0330

    Code GetForumByPermalink

--- Source/Services/Bargeh.Forums.Api/Services/ForumsService.cs
+++ Source/Services/Bargeh.Forums.Api/Services/ForumsService.cs
@@ -1,8 +1,26 @@
+using Bargeh.Forums.Api.Infrastructure;
+using Bargeh.Forums.Api.Infrastructure.Models;
 using Forums.Api;
+using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bargeh.Forums.Api.Services;
 
-public class ForumsService : ForumsProto.ForumsProtoBase
+public class ForumsService(ForumsDbContext dbContext) : ForumsProto.ForumsProtoBase
 {
-	
+	public override async Task<ForumReply> GetForumByPermalink(GetForumByPermalinkRequest request,
+															   ServerCallContext context)
+	{
+		Forum forum = await dbContext.Forums.FirstOrDefaultAsync(f => f.Permalink == request.Permalink)
+					  ?? throw new RpcException(new(StatusCode.NotFound, "No forum was found with this permalink"));
+
+		return new()
+		{
+			Name = forum.Name,
+			Description = forum.Description,
+			Members = forum.Members,
+			Owner = forum.OwnerId.ToString(),
+			Supporters = forum.Supporters
+		};
+	}
 }
